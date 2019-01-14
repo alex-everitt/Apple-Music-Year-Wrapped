3f030927a5911c5a18da26d9aa4cd9ec983666ab
Alex Everitt <a.everitt09@gmail.com>
2019-01-14 15:20:34 -0500

    started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
+import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './components/Home/home.js';
 import Results from './components/Results/results.js';
--- src/components/Wrapped/wrapped.js
+++ src/components/Wrapped/wrapped.js
@@ -3,20 +3,44 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Image from '../../wrapped_background.jpg';
+import Chungus from '../../big_chungus.jpg';
+
+import CardMedia from '@material-ui/core/CardMedia'
+
+import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+function generate(element) {
+  return [0, 1, 2].map(value =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
 
 const box_style = {
-  padding: '30px'
+  padding: '30px',
+  height: '100%'
 };
 
 const paper_style = {
-  padding: '30px',
+  padding: '10px',
   backgroundImage: `url(${Image})`,
   backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat'
+  backgroundRepeat: 'no-repeat',
+  height: '100%',
+  width: '100%'
 };
 
 const text_style = {
   color: 'white'
+};
+
+const header_style = {
+  color: 'black',
+  backgroundColor: 'white',
+  fontStyle: 'italic'
 };
 
 class Wrapped extends Component {
@@ -25,115 +49,93 @@ class Wrapped extends Component {
     this.state = {
       playData : props.playData,
       topSongs : [],
-      topArtists : []
+      topArtists : [],
+      imageUrl:"https://s3.dexerto.com/articles/Big-Chungus.jpg"
     }
   }
 
   render() {
     console.log(this.props.totalPlaytime)
-    var topDivs = []
+    var songDivs = []
+    var artistDivs = []
             for (let index = 0; index < 10; index++) {
-              if(index === 0){
-                let div = <Grid container spacing={8} key={index}>
-                <Grid item xs ={4}>
-                    <Typography variant='h2' align='left' gutterBottom style={text_style}>
-                     {Math.floor(this.props.totalPlaytime)}
-                     </Typography>
-                </Grid>
-                <Grid item xs ={4}>
-                    <Typography variant='subtitle1' align='left' gutterBottom style={text_style}>
-                     {index+1}. {this.props.topSongs[index]} ({this.props.topSongPlays[index]} plays)
-                     </Typography>
-                </Grid>
-                <Grid item xs ={4}>
-                    <Typography variant='subtitle1' align='left' gutterBottom style={text_style}>
-                     {index+1}. {this.props.topArtists[index]} ({this.props.topArtistPlays[index]} plays)
-                     </Typography>
-                </Grid>
-              </Grid>
-              topDivs.push(div)
-              }
-              else{
-                  let div = <Grid container spacing={8} key={index}>
-                  <Grid item xs ={4}>
+              let songDiv= <ListItem>
+                    <ListItemText disableTypography
+                    primary={<Typography type="body2" style={{ color: '#FFFFFF' }}>{index+1}. {this.props.topSongs[index]} ({this.props.topSongPlays[index]} plays)</Typography>} />
+             </ListItem>
 
-                  </Grid>
-                  <Grid item xs ={4}>
-                      <Typography variant='subtitle1' align='left' gutterBottom style={text_style}>
-                       {index+1}. {this.props.topSongs[index]} ({this.props.topSongPlays[index]} plays)
-                       </Typography>
-                  </Grid>
-                  <Grid item xs ={4}>
-                      <Typography variant='subtitle1' align='left' gutterBottom style={text_style}>
-                       {index+1}. {this.props.topArtists[index]} ({this.props.topArtistPlays[index]} plays)
-                       </Typography>
-                  </Grid>
-                </Grid>
-                topDivs.push(div)
+             let artistDiv= <ListItem>
+                   <ListItemText disableTypography
+                   primary={<Typography type="body2" style={{ color: '#FFFFFF' }}>{index+1}. {this.props.topArtists[index]} ({this.props.topArtistPlays[index]} plays)</Typography>} />
+            </ListItem>
+
+              songDivs.push(songDiv)
+              artistDivs.push(artistDiv)
               }
-              //topDivs.push(div)
-    }
+     var imageURL = "url('"+this.state.imageURL+"')";
+
+    var image_style = {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      height: '50%',
+      width: '50%',
+      backgroundImage: imageURL,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat'
+    };
 
     return (
       <div style={box_style}>
-        <Paper style={paper_style}>
-          <Grid id="top-row" container spacing={8}>
-            <Grid item xs ={12}>
-                  <Typography variant='h3' align='center' gutterBottom style={text_style}>
-                   Your 2018 Wrapped
-                   </Typography>
-            </Grid>
+        <Paper zDepth={3} style={paper_style}>
+        <Grid id="top-row" container spacing={8} justify='center' alignItems="center" direction="column">
+          <Grid item xs ={8}>
+                <Typography variant='h3' align='center' gutterBottom style={header_style}>
+                 Your 2018 Wrapped
+                 </Typography>
           </Grid>
+        </Grid>
 
-          <Grid id="header-row" container spacing={8}>
-            <Grid item xs ={4}>
-              <Typography variant='h6' align='center' gutterBottom style={text_style}>
-                I Listened To
+        <Grid id="header-row" container spacing={2}>
+          <Grid item xs ={5}>
+            <Typography variant='h6' align='center' gutterBottom style={text_style}>
+              I Listened To
+            </Typography>
+            <Typography variant='h2' align='center' gutterBottom style={text_style}>
+             {Math.floor(this.props.totalPlaytime)}
+             </Typography>
+             <Typography variant='h6' align='center' gutterBottom style={text_style}>
+              minutes of music
               </Typography>
-            </Grid>
+
+              <Paper style={image_style}>
+              </Paper>
+
           </Grid>
 
-          <Grid id="h-row" container spacing={8}>
-            <Grid item xs ={4}>
-                  <Typography variant='h6' align='center' gutterBottom style={text_style}>
-                   Your Top Songs
-                   </Typography>
-            </Grid>
-          </Grid>
-          <Grid id="head-row" container spacing={8}>
-            <Grid item xs ={4}>
-                  <Typography variant='h6' align='center' gutterBottom style={text_style}>
-                   Your Top Artists
-                   </Typography>
-            </Grid>
+          <Grid item xs ={4}>
+                <Typography variant='h6' align='center' gutterBottom style={text_style}>
+                 Your Top Songs
+                 </Typography>
+                 <List >
+                  {songDivs}
+                 </List>
+
           </Grid>
 
-
-
+          <Grid item xs ={3}>
+                <Typography variant='h6' align='center' gutterBottom style={text_style}>
+                 Your Top Artists
+                 </Typography>
+                 <List >
+                  {artistDivs}
+                 </List>
+          </Grid>
+        </Grid>
 
         </Paper>
       </div>
     );
 }
-//{topDivs}
-    // <Grid id="info-row" container spacing={8}>
-    //   <Grid item xs ={4}>
-    //         <Typography variant='subtitle1' align='left' gutterBottom>
-    //          {this.props.topSongs[1]} ({this.props.topSongPlays[1]} plays)
-    //          {this.props.topSongs[2]} ({this.props.topSongPlays[2]} plays)
-    //
-    //          </Typography>
-    //   </Grid>
-    //   <Grid item xs ={4}>
-    //         <Typography variant='subtitle1' align='left' gutterBottom>
-    //          {this.props.topArtists[0]} ({this.props.topArtistPlays[0]} plays)
-    //          {this.props.topArtists[1]} ({this.props.topArtistPlays[1]} plays)
-    //          </Typography>
-    //   </Grid>
-    //   <Grid item xs ={4}>
-    //
-    //   </Grid>
-    // </Grid>
 
   }
 //}
